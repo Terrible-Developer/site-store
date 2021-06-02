15f5e49b98b9f200b9ff55c84dbcf6819ef06462
Victor Bruno Paro <victorbparo@gmail.com>
2021-06-02 03:19:04 -0300

    Descobrindo e resolvendo o problema das rotas darem 404 quando digitadas diretamente na barra

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const history = require('connect-history-api-fallback');
+
+app.use(history());
 app.use(express.static('dist'));
 
 app.listen(process.env.PORT || 3000, () => {
