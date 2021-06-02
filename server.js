b1e20e8f03deb2c86a9e480d57a1cbbc2f18698f
Victor Bruno Paro <victorbparo@gmail.com>
2021-06-02 02:06:12 -0300

    Recomeçando o site do zero, trocando quasar por vue vanilla, começando a usar vue3, fazendo páginas meme só por inicialização

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`testing, running on port ${process.env.PORT || 3000}`);
+});
